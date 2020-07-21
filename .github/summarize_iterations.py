dda6d3f50d82f3b33b9640c1a4110feeaac4b16b
rlskoeser <rebecca.s.koeser@princeton.edu>
2020-07-21 10:25:01 -0400

    Clean iteration summary code, calculate velocity

--- .github/summarize_iterations.py
+++ .github/summarize_iterations.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
 
-from datetime import datetime
-from datetime import date
-from collections import defaultdict
-import csv
-from datetime import date
-import pandas as pd
 import json
+from datetime import datetime
+
+import pandas as pd
 
 
 def summarize_iterations():
     """Divide the issues csv by iteration and by whether it contains the `design` label.
-    For dev and for design, summarize the story points and issues closed during 
+    For dev and for design, summarize the story points and issues closed during
     the each iteration. Save the output json to the `data` dir as iteration-summary.json.
     """
     df = pd.read_csv('data/issues.csv')
@@ -23,34 +20,43 @@ def summarize_iterations():
     df_dev = df_closed[~mentions_design]
 
     with open('data/iterations.json') as f:
-        iterations_json = json.load(f)
+        iteration_data = json.load(f)
 
-    # Build and save the summary CSV 
-    output_l = []
-    for iteration_d in iterations_json:
-        row = {}
-        
-        start_date = datetime.fromisoformat(iteration_d['from'])
-        end_date = datetime.fromisoformat(iteration_d['to'])
-        row['from'] = iteration_d['from']
-        row['to'] = iteration_d['to']
+    # Build and save the summary CSV
+    for iteration in iteration_data:
+        # don't recalculate values that are already present
+        if 'dev' in iteration:
+            continue
 
-        df_it = df_dev[(start_date <= df_dev['closed']) & (df_dev['closed'] <= end_date)]
-        row['dev'] = {
-            'story_points': float(df_it['estimate'].sum()),
-            'issues_closed': float(df_it['closed'].count())
-        }
-        
-        df_it = df_design[(start_date <= df_design['closed']) & (df_design['closed'] <= end_date)]
-        row['design'] = {
-            'story_points': float(df_it['estimate'].sum()),
-            'issues_closed': float(df_it['closed'].count())
+        start_date = datetime.strptime(iteration['from'], '%Y-%m-%d')
+        end_date = datetime.strptime(iteration['to'], '%Y-%m-%d')
+
+        df_it = df_dev[(start_date <= df_dev['closed']) &
+                       (df_dev['closed'] <= end_date)]
+        iteration['dev'] = {
+            'points': int(df_it['estimate'].sum()),
+            'issues': int(df_it['closed'].count())
         }
 
-        output_l.append(row)
+        df_it = df_design[(start_date <= df_design['closed']) &
+                          (df_design['closed'] <= end_date)]
+        iteration['design'] = {
+            'points': int(df_it['estimate'].sum()),
+            'issues': int(df_it['closed'].count())
+        }
+
+    # calculate velocity
+    for i, iteration in enumerate(iteration_data):
+        if i < 2:
+            continue
+        iteration['dev']['velocity'] = '%.2f' % \
+            (sum([i['dev']['points'] for i in iteration_data[i - 2:i]]) / 3.0)
+
+        iteration['design']['velocity'] = '%.2f' % \
+            (sum([i['design']['points'] for i in iteration_data[i - 2:i]]) / 3.0)
 
     with open('data/iteration-summary.json', 'w') as f:
-        json.dump(output_l, f, indent=4)
+        json.dump(iteration_data, f, indent=4)
 
 if __name__ == '__main__':
     summarize_iterations()
