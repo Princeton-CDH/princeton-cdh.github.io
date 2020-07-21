a8b3a725710cbf1a171cfd1df3ad92fea39b3760
rlskoeser <rebecca.s.koeser@princeton.edu>
2020-07-21 09:27:47 -0400

    Switch to repo exclude list; include milestone in csv

--- .github/get_issues.py
+++ .github/get_issues.py
@@ -1,61 +1,75 @@
 #!/usr/bin/env python
-from datetime import date, datetime
-import csv
+
+import datetime
 import os
+import subprocess
 
 import pandas as pd
 
-from storypoints import ZenHubAPI, GitHubAPI
+from storypoints import GitHubAPI, ZenHubAPI
 
-DEVTEAM_PROJECTS = [
-    'cdh-web', 'winthrop-django', 'django-pucas', 'djiffy', 'django-annotator-store',
-    'derrida-django', 'mep-django', 'viapy',
-    'ppa-django', 'parasolr', 'pemm-scripts', 'startwords'
+EXCLUDE_REPOS = [
+    'bitKlavier', 'mapping-expatriate-paris', 'davilajs',
+    'bluemountainsprings', 'bluemountain-transcriptions', 'mepApp',
+    'mep-beach-logbooks', 'abcbooks'
 ]
+
 
 def get_issues():
     """
-    Use ZenHub and GitHub API to compile a CSV of all issues in the DEVTEAM_PROJECTS
-    list. If the CSV already exists, find the latest date in the CSV and update 
-    as necessary. Place the file into the `data` dir as issues.csv
+    Use ZenHub and GitHub API to generate a CSV of all issues in
+    all repositories in this GitHub organization, excluding any in the
+    EXCLUDE_REPOS list. If the CSV already exists, use the file last
+    modification date and update based on issues modified since then.
+    File is saved as `data/issues.csv`.
     """
 
     # Get APIs
-    ghub = GitHubAPI(os.environ['GITHUB_USERNAME'], os.environ['GITHUB_API_TOKEN'])
+    ghub = GitHubAPI(os.environ['GITHUB_USERNAME'],
+                     os.environ['GITHUB_API_TOKEN'])
     zhub = ZenHubAPI(os.environ['ZENHUB_API_TOKEN'])
 
     # Read existing CSV
     filename = 'data/issues.csv'
-    if os.path.exists(filename):
-        df = pd.read_csv(filename, parse_dates=['closed'])
-        # find the latest date already read
-        date_since = df['closed'].max()
-        data_dict = df.to_dict('records')
-    else:
-        date_since = date(2020, 7, 1) # HELP: How far back do we want to look?
-        data_dict = []
+    date_since = None
+    data_dict = []
 
-    # get dict of repo by name -> id
-    repos = ghub.get_org_repos('Princeton-CDH')
+    if os.path.exists(filename):
+        # get the last modification of the file in git
+        last_commit_date = subprocess.check_output(
+            ['git', 'log', '--date=short', '--pretty=%ad', 'data/issues.csv'])
+        # convert bytes to string
+        last_commit_date = last_commit_date.strip().decode()
+        date_since = datetime.datetime.strptime(last_commit_date, '%Y-%m-%d')
+        df = pd.read_csv(filename, parse_dates=['closed'])
+        data_dict = df.to_dict('records')
+
+    github_org = os.environ['GITHUB_REPOSITORY'].split('/')[0]
+    repos = ghub.get_org_repos(github_org)
+
+    get_issue_params = {}
+    if date_since:
+        get_issue_params['since'] = date_since.isoformat()
 
     for repo, repo_id in repos.items():
-        if repo not in DEVTEAM_PROJECTS:
+        if repo in EXCLUDE_REPOS:
+            print('Skipping %s' % repo)
             continue
 
-        for issue in ghub.get_repo_issues('Princeton-CDH', repo,
-                                          params={'since': date_since.isoformat()}):
-            
+        for issue in ghub.get_repo_issues(github_org, repo,
+                                          params=get_issue_params):
             zhub_info = zhub.get_issue(repo_id, issue['number'])
-            
+            milestone = issue['milestone']
+
             info = {
                 'issue': issue['title'],
                 'project': repo,
                 'url': issue['url'],
                 'estimate': zhub_info.get('estimate', {}).get('value', ''),
                 'closed': issue['closed_at'],
-                'labels': ';'.join([label['name'] for label in issue['labels']])
+                'labels': ';'.join([l['name'] for l in issue['labels']]),
+                'milestone': milestone['title'] if milestone else '',
             }
-
             data_dict.append(info)
 
     pd.DataFrame(data_dict).to_csv(filename, index=False)
